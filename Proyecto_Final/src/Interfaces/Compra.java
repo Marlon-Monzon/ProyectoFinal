b398438cc7506f30ec568e916cb8d0c2a34d77a4
Marlon Monzon <marlonmonzoncanel@gmail.com>
2020-12-07 02:56:34 -0600

    Agregar Datos a la BD desde JTextField

--- Proyecto_Final/src/Interfaces/Compra.java
+++ Proyecto_Final/src/Interfaces/Compra.java
@@ -8,6 +8,10 @@ package Interfaces;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.DriverManager;
+
 
 /**
  *
@@ -37,22 +41,23 @@ public class Compra extends javax.swing.JFrame {
     private void initComponents() {
 
         Return = new javax.swing.JButton();
-        Buscar = new javax.swing.JButton();
+        Registrar = new javax.swing.JButton();
         Salir = new javax.swing.JButton();
-        JBuscar1 = new javax.swing.JTextField();
+        JNombre = new javax.swing.JTextField();
         JEjemplo = new javax.swing.JLabel();
-        JBuscar2 = new javax.swing.JTextField();
-        JBuscar3 = new javax.swing.JTextField();
-        JBuscar4 = new javax.swing.JTextField();
+        JAlmacen = new javax.swing.JTextField();
+        JGramos = new javax.swing.JTextField();
+        JPrecio = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jComboBox2 = new javax.swing.JComboBox<>();
+        JReceta = new javax.swing.JComboBox<>();
+        JVia = new javax.swing.JComboBox<>();
         jLabel7 = new javax.swing.JLabel();
+        JTrabajadores = new javax.swing.JComboBox<>();
         Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -70,16 +75,21 @@ public class Compra extends javax.swing.JFrame {
         });
         getContentPane().add(Return, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
 
-        Buscar.setBackground(new java.awt.Color(0, 0, 102));
-        Buscar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        Buscar.setForeground(new java.awt.Color(255, 255, 255));
-        Buscar.setText("Registrar");
-        Buscar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BuscarActionPerformed(evt);
+        Registrar.setBackground(new java.awt.Color(0, 0, 102));
+        Registrar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        Registrar.setForeground(new java.awt.Color(255, 255, 255));
+        Registrar.setText("Registrar");
+        Registrar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                RegistrarMouseClicked(evt);
             }
         });
-        getContentPane().add(Buscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 330, -1, 30));
+        Registrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RegistrarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(Registrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 330, -1, 30));
 
         Salir.setBackground(new java.awt.Color(0, 0, 102));
         Salir.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -91,15 +101,15 @@ public class Compra extends javax.swing.JFrame {
             }
         });
         getContentPane().add(Salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 20, -1, -1));
-        getContentPane().add(JBuscar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 170, 30));
+        getContentPane().add(JNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 120, 170, 30));
 
         JEjemplo.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         JEjemplo.setForeground(new java.awt.Color(255, 255, 255));
         JEjemplo.setText("Compra de medicamentos ingrese los datos correspondientes");
         getContentPane().add(JEjemplo, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 100, 380, -1));
-        getContentPane().add(JBuscar2, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 260, 170, 30));
-        getContentPane().add(JBuscar3, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 120, 170, 30));
-        getContentPane().add(JBuscar4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 190, 170, 30));
+        getContentPane().add(JAlmacen, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 260, 170, 30));
+        getContentPane().add(JGramos, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 120, 170, 30));
+        getContentPane().add(JPrecio, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 190, 170, 30));
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
@@ -131,20 +141,23 @@ public class Compra extends javax.swing.JFrame {
         jLabel6.setText("Nombre del medicamento");
         getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, 170, 20));
 
-        jComboBox1.setBackground(new java.awt.Color(0, 0, 102));
-        jComboBox1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jComboBox1.setForeground(new java.awt.Color(255, 255, 255));
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Si", "No", " " }));
-        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 170, 30));
+        JReceta.setBackground(new java.awt.Color(0, 0, 102));
+        JReceta.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        JReceta.setForeground(new java.awt.Color(255, 255, 255));
+        JReceta.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Si", "No", " " }));
+        getContentPane().add(JReceta, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 170, 30));
 
-        jComboBox2.setBackground(new java.awt.Color(0, 0, 102));
-        jComboBox2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jComboBox2.setForeground(new java.awt.Color(255, 255, 255));
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Oral", "Sublingual", "Topica", "Parental", "Respiratoria", "Rectal", "Vaginal", " " }));
-        getContentPane().add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 190, 170, 30));
+        JVia.setBackground(new java.awt.Color(0, 0, 102));
+        JVia.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        JVia.setForeground(new java.awt.Color(255, 255, 255));
+        JVia.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Oral", "Sublingual", "Topica", "Parental", "Respiratoria", "Rectal", "Vaginal", " " }));
+        getContentPane().add(JVia, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 190, 170, 30));
 
         jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Logo2.png"))); // NOI18N
         getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 0, -1, -1));
+
+        JTrabajadores.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione", "1" }));
+        getContentPane().add(JTrabajadores, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 60, 150, 30));
 
         Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/OIP (1).jpg"))); // NOI18N
         getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 370));
@@ -153,9 +166,9 @@ public class Compra extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void BuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BuscarActionPerformed
+    private void RegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegistrarActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_BuscarActionPerformed
+    }//GEN-LAST:event_RegistrarActionPerformed
 
     private void SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalirActionPerformed
      JOptionPane.showMessageDialog(null, "Cierre de sesion automatico activado", "Cerrar Aplicacion",JOptionPane.CANCEL_OPTION);
@@ -167,6 +180,31 @@ public class Compra extends javax.swing.JFrame {
         v.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_ReturnActionPerformed
+
+    private void RegistrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RegistrarMouseClicked
+        Connection con;
+        CallableStatement cs;
+        try {
+           Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Farmacia","Marlon","mpsaaj");
+             cs = con.prepareCall("select insertarregistrofull(?,?,?,?,?,?,?)");
+             cs.setString(1, this.JNombre.getText());
+             cs.setInt(2, Integer.parseInt((String)this.JGramos.getText()));
+             cs.setInt(3, Integer.parseInt((String)this.JPrecio.getText()));
+             cs.setString(4, (String) this.JVia.getSelectedItem());
+             cs.setString(5, (String) this.JReceta.getSelectedItem());
+             cs.setInt(6, Integer.parseInt((String)this.JAlmacen.getText()));
+             cs.setInt(7, Integer.parseInt((String)this.JTrabajadores.getSelectedItem()));
+             if (cs.execute()) {
+                JOptionPane.showMessageDialog(getParent(), "Compra Realizada Correctamente");
+                MenuPrincipal v = new MenuPrincipal();
+                v.setVisible(true);
+                this.setVisible(false);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }//GEN-LAST:event_RegistrarMouseClicked
 
     /**
      * @param args the command line arguments
@@ -205,17 +243,18 @@ public class Compra extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Buscar;
     private javax.swing.JLabel Fondo;
-    private javax.swing.JTextField JBuscar1;
-    private javax.swing.JTextField JBuscar2;
-    private javax.swing.JTextField JBuscar3;
-    private javax.swing.JTextField JBuscar4;
+    private javax.swing.JTextField JAlmacen;
     private javax.swing.JLabel JEjemplo;
+    private javax.swing.JTextField JGramos;
+    private javax.swing.JTextField JNombre;
+    private javax.swing.JTextField JPrecio;
+    private javax.swing.JComboBox<String> JReceta;
+    private javax.swing.JComboBox<String> JTrabajadores;
+    private javax.swing.JComboBox<String> JVia;
+    private javax.swing.JButton Registrar;
     private javax.swing.JButton Return;
     private javax.swing.JButton Salir;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
